a9f2d9c4bceb9d0bf84e42b1f504ed3cc1a20609
김찬우 <chanwoo040531@gmail.com>
2024-05-23 17:41:47 +0900

    [chore] remove security dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,13 +19,11 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-websocket")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.springframework.security:spring-security-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
